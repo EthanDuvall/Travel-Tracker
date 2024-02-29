cb605472f867234aeacf699546a0b2b0825579b7
Ethan Duvall <eduvall23@turing.edu>
2024-02-29 12:40:24 -0700

    fetch/Trips-Users

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,11 @@
-
-function fetchUserData(id,usersData) {
+function fetchUserData(id, usersData) {
   return fetch(`http://localhost:3001/api/v1/travelers/${id} `)
     .then((rsp) => rsp.json())
     .then((data) => (usersData = data));
 }
-
-export {fetchUserData}
+function fetchTripData(tripData) {
+  return fetch("http://localhost:3001/api/v1/trips")
+    .then((rsp) => rsp.json())
+    .then((data) => (tripData = data));
+}
+export { fetchUserData, fetchTripData };
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,18 +1,25 @@
-import { fetchUserData,fetchTripData } from "./apiCalls";
-let user = "travel50";
-let userData;
-
-
+import { fetchUserData, fetchTripData } from "./apiCalls";
+let user = "travel1";
+let userData, userTrips;
 
 window.addEventListener("load", updatePage);
 
-
 function updatePage() {
-  Promise.all([fetchUserData(1)]).then((fetchUser) => {
-    userData = fetchUser
-    console.log(userData)
-    console.log("hi");
+  Promise.all([fetchUserData(getUserId()), fetchTripData()]).then(
+    ([fetchUser, fetchTrip]) => {
+      userData = fetchUser;
+      userTrips = getUsersTrips(fetchTrip);
+    }
+  );
+}
+
+function getUsersTrips(fetchTrip) {
+  return fetchTrip.trips.filter((tripData) => {
+    return tripData.userID === userData.id;
   });
 }
 
-function showAllTravels() {}
+function getUserId() {
+  let userId = user.split("l");
+  return userId[1];
+}
