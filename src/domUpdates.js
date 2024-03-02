152f200fdc19d2c02cacadb3a6074b21d7d87f77
Ethan Duvall <eduvall23@turing.edu>
2024-03-02 02:35:30 -0700

    Display users Trips

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,47 +1,59 @@
-import { fetchUserData, fetchTripData } from "./apiCalls";
-import { getUserId, getUsersTrips } from "./user";
+import { fetchUserData, fetchTripData,fetchDestinationsData } from "./apiCalls";
+import { getUserId, getUsersTrips, getDestination} from "./user";
 
 const pastButton = document.querySelector("#pastButton")
 const pendingButton = document.querySelector("#pendingButton")
 const tabs = document.querySelector("#tabSwap")
 const pastTab = document.querySelector("#past")
-
+const pendingTab= document.querySelector("#pending")
 let selectedButton = pastButton
 let selectedTab = pastTab
-let user = "traveler1";
-let userData, userTrips;
-/*
-<aside>
-<div>
-<h4>${trip.name}</h4>
-<p></p>
-<p></p>
-</div>
-<img src =${trip.img} alt = ${trip.alt}>
-</aside>
-*/
+let user = "traveler7";
+let userData, userTrips,destinations;
+
 window.addEventListener("load", updatePage);
 
 tabs.addEventListener("click",evt => {
   let tab = evt.target.closest("button")
   let selectedSection = document.querySelector(`#${tab.classList[0]}`)
   toggleTabs(selectedSection,tab)
-  displayTrips(selectedSection)
 })
 
 
 function updatePage() {
-  Promise.all([fetchUserData(getUserId(user)), fetchTripData()]).then(
-    ([fetchUser, fetchTrip]) => {
+  Promise.all([fetchUserData(getUserId(user)), fetchTripData(),fetchDestinationsData()]).then(
+    ([fetchUser, fetchTrip, fetchDestinations]) => {
       userData = fetchUser;
+      console.log(fetchDestinations)
+      destinations = fetchDestinations.destinations
       userTrips = getUsersTrips(fetchTrip.trips,userData.id);
+      displayTrips()
     }
   );
 }
 
-function displayTrips(tab){
-  let section = document.querySelector(`#${tab.id}Trips`)
-  console.log(section)
+function displayTrips(){
+  let tab
+  let destination
+  userTrips.forEach(trip => {
+    if(trip.status ==="approved"){
+      tab = pastTab
+    }else if (trip.status === "pending"){
+      tab = pendingTab
+    }
+    destination = getDestination(trip.destinationID,destinations)
+    tab.insertAdjacentHTML("beforeend",
+      `
+      <div>
+      <aside>
+      <h4>${destination.destination}</h4>
+      <p>Travelers : ${trip.travelers}</p>
+      <p>${trip.date}</p>
+      </aside>
+      <img src =${destination.image} alt = ${destination.alt}>
+      </div>
+      `)
+  });
 }
 
 function toggleTabs(tab,button){
