cb1e6a756a0c6612d1213546c56421d201856aec
Ethan Duvall <eduvall23@turing.edu>
2024-03-02 16:47:46 -0700

    Working post request finalized

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,22 +13,22 @@ function fetchDestinationsData (destinations){
     .then((rsp) => rsp.json())
     .then((data) => (destinations = data));
 }
-function postTrip(trip){
-  return fetch('url',{
-    method: 'POST',
-    body: JSON.stringify({
-      id:trip.id,
-      userID:trip.userId,
-      destinationID:trip.destination,
-      travelers:trip.travelers,
-      date:trip.date,
-      duration:trip.duration,
-      status:'pending',
-      suggestedActivities:[]
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-}
+  function postTrip(trip){
+    return fetch('http://localhost:3001/api/v1/trips',{
+      method: 'POST',
+      body: JSON.stringify({
+        id:trip.id,
+        userID:trip.userId,
+        destinationID:trip.destination,
+        travelers:trip.travelers,
+        date:trip.date,
+        duration:trip.duration,
+        status:'pending',
+        suggestedActivities:[]
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 export { fetchUserData, fetchTripData, fetchDestinationsData,postTrip };
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,87 +1,105 @@
-import { fetchUserData, fetchTripData,fetchDestinationsData } from "./apiCalls";
-import { getUserId, getUsersTrips, getDestination,getTripExpenses,filterTrips} from "./user";
+import {
+  fetchUserData,
+  fetchTripData,
+  fetchDestinationsData,
+  postTrip,
+} from "./apiCalls";
+import {
+  getUserId,
+  getUsersTrips,
+  getDestination,
+  getTripExpenses,
+  filterTrips,
+} from "./user";
 
-const pastButton = document.querySelector("#pastButton")
-const pendingButton = document.querySelector("#pendingButton")
-const tabs = document.querySelector("#tabSwap")
-const pastTab = document.querySelector("#past")
-const pendingTab= document.querySelector("#pending")
-const expensesBox = document.querySelectorAll(".expenses")
-const popUpForm = document.querySelector (".pop-up-form")
-const addTripButton = document.querySelector(".addTrip")
-const displayBoxes = document.querySelectorAll(".box")
-const closeButton = document.querySelector(".closeBtn")
-const formDisplayArea = document.querySelector("#formDisplay")
-const dateInput = document.querySelector("#dateInput")
-const desnationInput = document.getElementsByName("destination")
-const travelerInput = document.querySelector("#travelersInput")
-const durationInput = document.querySelector("#durationInput")
-const findButton = document.querySelector(".formBtn")
-const submitButton = document.querySelector(".submitBtn")
+const pastButton = document.querySelector("#pastButton");
+const pendingButton = document.querySelector("#pendingButton");
+const tabs = document.querySelector("#tabSwap");
+const pastTab = document.querySelector("#past");
+const pendingTab = document.querySelector("#pending");
+const expensesBox = document.querySelectorAll(".expenses");
+const popUpForm = document.querySelector(".pop-up-form");
+const addTripButton = document.querySelector(".addTrip");
+const displayBoxes = document.querySelectorAll(".box");
+const closeButton = document.querySelector(".closeBtn");
+const formDisplayArea = document.querySelector("#formDisplay");
+const dateInput = document.querySelector("#dateInput");
+const desnationInput = document.getElementsByName("destination");
+const travelerInput = document.querySelector("#travelersInput");
+const durationInput = document.querySelector("#durationInput");
+const findButton = document.querySelector(".formBtn");
+const submitButton = document.querySelector(".submitBtn");
 
-
-let selectedButton = pastButton
-let selectedTab = pastTab
+let selectedButton = pastButton;
+let selectedTab = pastTab;
 let user = "traveler7";
-let userData, userTrips,destinations,allTrips;
+let userData, userTrips, destinations, allTrips;
 
 window.addEventListener("load", updatePage);
 
-addTripButton.addEventListener("click",openAddTripForm)
-closeButton.addEventListener("click",closeAddTripForm)
+addTripButton.addEventListener("click", openAddTripForm);
+closeButton.addEventListener("click", closeAddTripForm);
 
-findButton.addEventListener("click",evt =>{
-  evt.preventDefault()
-  displayAviableBookings()
-})
-submitButton.addEventListener("click",()=>{
-  let desnation = getCheckedDesntionation()
-  let newTrip ={
-    id:Date.now,
-    userId:userData.id,
-
-  }
-})
-tabs.addEventListener("click",evt => {
-  let tab = evt.target.closest("button")
-  let selectedSection = document.querySelector(`#${tab.classList[0]}`)
-  toggleTabs(selectedSection,tab)
-})
-
+findButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  displayAviableBookings();
+});
+submitButton.addEventListener("click", (evt) => {
+  let selectedDestionation = getCheckedDesntionation();
+  let desnationId = Number(selectedDestionation.id)
+  let newTrip = {
+    id: Date.now(),
+    userId: userData.id,
+    destination: desnationId,
+    travelers: travelerInput.value,
+    date: dateInput.value.replaceAll("-", "/"),
+    duration: durationInput.value,
+  };
+  postTrip(newTrip)
+});
+tabs.addEventListener("click", (evt) => {
+  let tab = evt.target.closest("button");
+  let selectedSection = document.querySelector(`#${tab.classList[0]}`);
+  toggleTabs(selectedSection, tab);
+});
 
 function updatePage() {
-  Promise.all([fetchUserData(getUserId(user)), fetchTripData(),fetchDestinationsData()]).then(
-    ([fetchUser, fetchTrip, fetchDestinations]) => {
-      userData = fetchUser;
-      allTrips = fetchTrip.trips
-      destinations = fetchDestinations.destinations
-      userTrips = getUsersTrips(fetchTrip.trips,userData.id);
-      displayTrips()
-      displayExpenses()
-      console.lo
-    }
-  );
+  Promise.all([
+    fetchUserData(getUserId(user)),
+    fetchTripData(),
+    fetchDestinationsData(),
+  ]).then(([fetchUser, fetchTrip, fetchDestinations]) => {
+    userData = fetchUser;
+    allTrips = fetchTrip.trips;
+    destinations = fetchDestinations.destinations;
+    userTrips = getUsersTrips(fetchTrip.trips, userData.id);
+    displayTrips();
+    displayExpenses();
+  });
 }
-function getStoredUser(){
+function getStoredUser() {
   //sessionStorage.setItem("user",userInput.value)
   //sessionStorage.getItem("user")
 }
-function getCheckedDesntionation(){
-  return desnationInput.find(destination => {
-    return destination.checked
-  })
+function getCheckedDesntionation() {
+  let foundDestionation;
+  desnationInput.forEach((destination) => {
+    if (destination.checked) foundDestionation = destination;
+  });
+  return foundDestionation;
 }
-function displayTrips(){
-  let tab
-  let destination
-  userTrips.forEach(trip => {
-    if(trip.status ==="approved"){
-      tab = pastTab
-    }else if (trip.status === "pending"){
-      tab = pendingTab
+function displayTrips() {
+  let tab;
+  let destination;
+  userTrips.forEach((trip) => {
+    if (trip.status === "approved") {
+      tab = pastTab;
+    } else if (trip.status === "pending") {
+      tab = pendingTab;
     }
-    destination = getDestination(trip.destinationID,destinations)
-    tab.insertAdjacentHTML("beforeend",
+    destination = getDestination(trip.destinationID, destinations);
+    tab.insertAdjacentHTML(
+      "beforeend",
       `
       <div>
       <aside>
@@ -91,70 +109,80 @@ function displayTrips(){
       </aside>
       <img src =${destination.image} alt = ${destination.alt}>
       </div>
-      `)
+      `
+    );
   });
 }
-function displayExpenses(){
-  let expenses = getTripExpenses(destinations,userTrips)
-  expensesBox.forEach(expense =>{
-    let key = expense.id + "Cost"
-    expense.insertAdjacentHTML("beforeend",`<p>${expenses[key]}$</p>`)
-  })
+function displayExpenses() {
+  let expenses = getTripExpenses(destinations, userTrips);
+  expensesBox.forEach((expense) => {
+    let key = expense.id + "Cost";
+    expense.insertAdjacentHTML("beforeend", `<p>${expenses[key]}$</p>`);
+  });
 }
-function toggleTabs(tab,button){
-  selectedTab.classList.add("inactive")  
-  selectedTab.classList.remove("active")
-  tab.classList.add("active")
-  tab.classList.remove("inactive")
-  selectedButton.classList.remove("active")
-  button.classList.add("active")
-  selectedTab = tab
-  selectedButton = button
+function toggleTabs(tab, button) {
+  selectedTab.classList.add("inactive");
+  selectedTab.classList.remove("active");
+  tab.classList.add("active");
+  tab.classList.remove("inactive");
+  selectedButton.classList.remove("active");
+  button.classList.add("active");
+  selectedTab = tab;
+  selectedButton = button;
 }
 
-function openAddTripForm(){
-  popUpForm.style.display = "flex"
+function openAddTripForm() {
+  popUpForm.style.display = "flex";
   displayBoxes.forEach((box) => {
     box.style.filter = "blur(6px)";
   });
 }
 
-function closeAddTripForm(){
-  popUpForm.style.display = "none"
+function closeAddTripForm() {
+  popUpForm.style.display = "none";
   displayBoxes.forEach((box) => {
     box.style.filter = "blur(none)";
   });
 }
 
-function displayAviableBookings(){
-
+function displayAviableBookings() {
   const tripVaules = {
-    date:(dateInput.value).replaceAll("-","/"),
-    traveler:travelerInput.value,
-    duration:durationInput.value
-  }
-  displayDestinations(tripVaules)
-  submitButton.classList.remove('inactive')
+    date: dateInput.value.replaceAll("-", "/"),
+    traveler: travelerInput.value,
+    duration: durationInput.value,
+  };
+  displayDestinations(tripVaules);
+  submitButton.classList.remove("inactive");
 }
 
-
-function displayDestinations(tripVaule){
-let total = 0
-  destinations.forEach(desntination =>{
-    total = Math.round(((desntination.estimatedLodgingCostPerDay *tripVaule.duration) + (desntination.estimatedFlightCostPerPerson *  tripVaule.traveler))*.1)
-    formDisplayArea.insertAdjacentHTML("beforeend",`
+function displayDestinations(tripVaule) {
+  let total = 0;
+  destinations.forEach((desntination) => {
+    total = Math.round(
+      (desntination.estimatedLodgingCostPerDay * tripVaule.duration +
+        desntination.estimatedFlightCostPerPerson * tripVaule.traveler) *
+        0.1
+    );
+    formDisplayArea.insertAdjacentHTML(
+      "beforeend",
+      `
     <div>
       <label for="destination">${desntination.destination}</label>
       <input type="radio" name="destination" id="${desntination.id}" required/>
       <aside>
-        <p>Lodging Cost : ${desntination.estimatedLodgingCostPerDay *tripVaule.duration } $</p>
-        <p>Flight Cost : ${desntination.estimatedFlightCostPerPerson *  tripVaule.traveler}$</p>
+        <p>Lodging Cost : ${
+          desntination.estimatedLodgingCostPerDay * tripVaule.duration
+        } $</p>
+        <p>Flight Cost : ${
+          desntination.estimatedFlightCostPerPerson * tripVaule.traveler
+        }$</p>
         <p>Estimated Total : ${total} $</p>
       </aside>
       <img src = ${desntination.image} alt = ${desntination.alt}>
     </div>
-    `)
-  })
+    `
+    );
+  });
 }
 /*
 <div>
